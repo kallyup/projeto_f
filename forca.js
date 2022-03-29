dfde02add27c78cbce8c20fb829e1b3edf506861
perkles <deboramacedo8816@gmail.com>
2022-03-29 20:56:07 -0300

    minhas atualizações

--- forca.js
+++ forca.js
@@ -13,3 +13,13 @@ function adicionar(){
     email.value="",
     console.log(usuario);
 }
+
+let frutas =[pitaya, banana, morango, laranja, kiwi, caqui, ciriguela, macaiba, ameixa, goiaba, maracuja, melancia]
+let personagens= [naruto, goku, hulk, batman, aquaman, thor, popay, chapolim, levi, sasuke, pan, sakura]
+let cor= [rosa, azul, branco, amarelo, anil, vermelho, laranja, vinho, roxo, verde, preto, laranja]
+let paises= [brazil, argentina, uruguai, paraguai, chile, canada, frança, ucrania, russia, argelia, cuba, inglaterra]
+
+let temas=[frutas, paises, personagens, cor]
+
+
+
