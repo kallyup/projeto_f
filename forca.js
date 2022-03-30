ca6dbf9fad67fe316fad60182e8eab73b27a9c10
perkles <deboramacedo8816@gmail.com>
2022-03-30 14:17:32 -0300

    escolhendo tema e sorteando palavra

--- forca.js
+++ forca.js
@@ -13,13 +13,44 @@ function adicionar(){
     email.value="",
     console.log(usuario);
 }
-
-let frutas =[pitaya, banana, morango, laranja, kiwi, caqui, ciriguela, macaiba, ameixa, goiaba, maracuja, melancia]
-let personagens= [naruto, goku, hulk, batman, aquaman, thor, popay, chapolim, levi, sasuke, pan, sakura]
-let cor= [rosa, azul, branco, amarelo, anil, vermelho, laranja, vinho, roxo, verde, preto, laranja]
-let paises= [brazil, argentina, uruguai, paraguai, chile, canada, frança, ucrania, russia, argelia, cuba, inglaterra]
+let frutas =['pitaya', 'banana', 'morango', 'tangirina', 'kiwi', 'caqui', 'ciriguela', 'macaiba', 'ameixa', 'goiaba', 'maracuja', 'melancia']
+let personagens= ['naruto', 'goku', 'hulk', 'batman', 'aquaman', 'thor', 'popay', 'chapolim', 'levi', 'sasuke', 'pan', 'sakura']
+let cor= ['rosa', 'azul', 'branco', 'amarelo', 'anil', 'vermelho', 'laranja', 'vinho', 'roxo', 'verde', 'preto', 'laranja']
+let paises= ['brasil', 'argentina', 'uruguai', 'paraguai', 'chile', 'canada', 'frança', 'ucrania', 'russia', 'argelia', 'cuba', 'inglaterra']
 
 let temas=[frutas, paises, personagens, cor]
+console.log(frutas)
 
+let letrasErradas=[]
+let letasCertas=[]
 
+console.log(temas)
 
+function jogo(){
+    iniciar();
+    confereLetras();
+    contadorVD();
+    reset();
+
+}
+    function iniciar(){
+        let temaEscolhido = document.getElementById("tema").value
+        if(temaEscolhido=="frutas"){
+            console.log(`${frutas[Math.floor(Math.random()*frutas.length)]}`) 
+        }else if(temaEscolhido=="cor"){
+            console.log(`${cor[Math.floor(Math.random()*cor.length)]}`) 
+        }else if(temaEscolhido=="paises"){
+            console.log(`${paises[Math.floor(Math.random()*paises.length)]}`)
+        }else{
+            console.log(`${personagens[Math.floor(Math.random()*personagens.length)]}`) 
+        }
+    }
+    function confereLetras(){
+         
+    }
+    function contadorVD(){
+         
+    }
+    function reset(){
+         
+    }
